c3037b038a82a0b9504b093582a5dab9e65ea6db
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-06-06 12:52:27 +0530

    faq done

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -22,14 +22,11 @@ const Faq = () => {
                     <div className="w-full">
                         {FAQ_DATA.map((item, index) => (
                             <div key={index} className={`border-b-2 border-black p-[15px] sm:px-[30px] sm:pt-6 sm:pb-[20px] md:pt-8 md:pb-10 md:pr-[44px] ${openIndex === index ? 'md:pb-[24px]' : ''}`}>
-                                <button onClick={() => toggleAccordion(index)} className="w-full cursor-pointer flex justify-between items-center text-start font-semibold text-black text-2xl max-md:text-xl max-sm:text-lg" >
+                                <button onClick={() => toggleAccordion(index)} className="w-full outline-0 cursor-pointer flex justify-between items-center text-start font-semibold text-black text-2xl max-md:text-xl max-sm:text-lg" >
                                     <span>{item.heading}</span>
-                                    <span className="ml-4 relative w-4 h-4">
-                                        <span className="absolute top-1/2 left-0 w-full h-[2px] bg-black transform -translate-y-1/2 transition-all duration-300"></span>
-                                        <span className={`absolute top-0 left-1/2 h-full w-[2px] bg-black transform -translate-x-1/2 transition-all duration-300 ${openIndex === index ? 'rotate-180 opacity-0' : ''}`}></span>
-                                    </span>
+                                    <span className={`min-w-2 min-h-0.5 sm:min-w-0.5 sm:min-h-3 bg-black relative after:absolute after:w-full after:h-full after:bg-black after:rotate-90 after:transition-all after:duration-400  ${openIndex === index ? 'after:hidden sm:rotate-90' : ''}`}></span>
                                 </button>
-                                <Description descriptionClass={`text-base leading-[150%] overflow-hidden transition-all duration-300 max-w-[992px] ${openIndex === index ? 'max-h-screen pt-4' : 'max-h-0'}`} descriptionText={"NFT stands for “Non-fungible token,” which means that it’s a unique, digital item with blockchain-managed ownership that users can buy, own, and trade. Some NFT’s fundamental function is to be digital art. But they can also offer additional benefits like exclusive access to websites, event tickets, game items, and ownership records for physical objects. Think of it as a unique piece of art that can also work as a “members-only” card. Hustlin' Hardos works like this"} />
+                                <Description descriptionClass={`text-base leading-[150%] overflow-hidden transition-all duration-700 ease-in-out max-w-[992px] ${openIndex === index ? 'max-h-[330px] pt-4 opacity-100' : 'max-h-0 pt-0 opacity-0'}`} descriptionText={"NFT stands for “Non-fungible token,” which means that it’s a unique, digital item with blockchain-managed ownership that users can buy, own, and trade. Some NFT’s fundamental function is to be digital art. But they can also offer additional benefits like exclusive access to websites, event tickets, game items, and ownership records for physical objects. Think of it as a unique piece of art that can also work as a “members-only” card. Hustlin' Hardos works like this"} />
                             </div>
                         ))}
                     </div>
