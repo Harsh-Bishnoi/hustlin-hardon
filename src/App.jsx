cea86f163ae627d0e53c978b94fbd4d4155dfe14
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-06-06 12:56:31 +0530

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import BreakingNews from './components/BreakingNews'
 import Faq from './components/Faq'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import HeaderSlider from './components/HeaderSlider'
 import Hero from './components/Hero'
@@ -20,6 +21,7 @@ function App() {
       <Minting />
       <Team />
       <Faq />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="max-w-[1140px] mx-auto">
+            <p className='leading-[100%] text-center pt-6 sm:pt-8 pb-6'>© Hustlin' Hardos 2025</p>
+        </div>
+    )
+}
+
+export default Footer
